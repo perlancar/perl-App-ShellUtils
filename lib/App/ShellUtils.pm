71392cddc0414a4dbe4a26b838f6b008ca9eb430
perlancar (@pc-office) <perlancar@gmail.com>
2016-10-21 12:20:26 +0700

    Initial commit

---
+++ lib/App/ShellUtils.pm
@@ -0,0 +1,18 @@
+package App::ShellUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to shells
+
+=head1 DESCRIPTION
+
+This distribution contains the following CLI utilities:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
